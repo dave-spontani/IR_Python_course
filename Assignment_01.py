520da1f5d565372ac1bb56ea10ba92a5e114f632
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2022-09-16 10:47:34 +0200

    Add files via upload
    The first Python Assignment - this assignment will be graded and needs to be handed in until the 20th of September, 23:00.

---
+++ Assignment_01.py
@@ -0,0 +1,39 @@
+################################Assignment number one: Basic Python ##################################################
+
+##########Task Number one: Create a variable containing a string that is a number (e.g "9"). Cast the string as an integer
+
+
+
+##########Task Number two: Now, take that number and multiply it with any integer above 5 - save the 
+######### result in a variable named "multiplied"
+
+
+
+##########Task Number three: Create an empty list, then use a for-loop to fill the list with all the numbers leading up 
+######## to the number you created in "multiplied" (use "range()" and "append()"). Be sure to include 0!
+
+
+
+##########Task number four: Now do the same, but using a list comprehension 
+
+
+
+##########Task number five: Generate a list of letters from the alphabet (e.b ["a", "b", "c"...] etc.) that has the 
+######## same length as the list of numbers you created in Task three (use List slicing for this) if the length of the
+######## numbers list is below 26. Otherwise, Slice the numbers list to have as many elements as letters in the alphabet
+
+
+
+
+#########Task number six: Now that both lists are of the same length, write a double for-loop that prints out all elements 
+######## from both lists in the following format: a0, b0, c0 ..... a1, b1, c1.....
+
+
+
+####Extra credit
+#########Task number seven: Since you have both lists, we could write a dictionary that shows the position of a letter
+######## in the alphabet. Create an empty dict, then use a for-loop to populate it with the number as the key and the
+######## corresponding letter as the value, so e.g: 0 maps to "a", 1 maps to b etc,. Write it in five lines of code and 
+######## then print out the result that should look like this: {0: 'a', 1: 'b', 2: 'c', 3: 'd'.....}
+
+
